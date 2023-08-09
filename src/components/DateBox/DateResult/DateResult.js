c4f9b64d9838a05afead1b4843290b9a6c1c898c
Soo <soojlee0106@naver.com>
2023-08-10 00:21:34 +0900

    Add styling for components
    Closes #20

--- src/components/DateBox/DateResult/DateResult.js
+++ src/components/DateBox/DateResult/DateResult.js
@@ -1,15 +1,16 @@
 import React from 'react';
+import './DateResult.css';
 
 const DateResult = ({ experiences, totalExperience }) => {
     return (
         <div className="date-result">
-            <h2> Experiences </h2>
-            <ul>
+            <h2 className="date-result-h2"> Experiences </h2>
+            <ul className="date-result-ul">
                 {experiences.map((experience, index) => (
                     <li key={index}>{experience}</li>
                 ))}
             </ul>
-            <h2> Total Experience</h2>
+            <h2 className="date-result-h2"> Total Experience</h2>
             <p
                 id="total-experience"
                 data-testid="total-experience">
